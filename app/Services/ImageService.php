05f9e374dbcd6172afcd17f062896595b2999bd7
oseminikhin <oseminikhin@tolkit.top>
2023-03-18 11:36:56 +0300

    ImageService update

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -29,10 +29,11 @@ class ImageService
             $extention = self::EXTENTIONS[$image->mime()];
             $additionalType = $extention != 'webp' ? 'webp' : 'jpg';
 
-            self::createWidthSet($image, $name, $folderPath, $additionalType);
-
             self::saveImage(Image::make($image), $name, $folderPath . '/', $extention);
             self::saveImage(Image::make($image)->encode($additionalType), $name, $folderPath . '/', $additionalType);
+
+            self::createWidthSet($image, $name, $folderPath, $additionalType);
+
 
             return [$name . '.' . $extention, $name . '.' . $additionalType];
 
@@ -61,8 +62,8 @@ class ImageService
         foreach (self::SIZES as $sizeName => $size) {
             if (!($width > $size)) continue;
             $path = $folderPath . '/' . $sizeName . '/';
-            self::saveImage(Image::make($image)->widen($size), $name, $path, self::EXTENTIONS[$image->mime()]);
-            self::saveImage(Image::make($image)->encode($additionalType)->widen($size), $name, $path, self::EXTENTIONS[$image->mime()]);
+            // self::saveImage(Image::make($image)->widen($size), $name, $path, self::EXTENTIONS[$image->mime()]);
+            self::saveImage(Image::make($image)->encode("webp")->widen($size), $name, $path, self::EXTENTIONS[$image->mime()]);
         }
     }
 
