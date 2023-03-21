10c1acb0e4ca8e585e045b1d3035f35eb6bbf7ae
oseminikhin <oseminikhin@tolkit.top>
2023-03-21 10:03:10 +0300

    temp refactor image service

--- app/Services/ImageSetService.php
+++ app/Services/ImageSetService.php
@@ -6,8 +6,30 @@ use Exception;
 use Intervention\Image\Facades\Image;
 use \App\Contracts\ImageContract;
 
-class ImageSetService implements ImageContract
+class ImageSetService
 {
+    const EXTENTIONS = [
+        "image/png" => 'png',
+        "image/jpeg" => 'jpg',
+        "image/jpg" => 'jpg',
+        "image/webp" => 'webp'
+    ];
+
+    const MIMES = [
+        'png' => "image/png",
+        'jpeg' => "image/jpeg",
+        'jpg' => "image/jpg",
+        'webp' => "image/webp"
+    ];
+
+    const SIZES = [
+        'sm' => 576,
+        'md' => 992,
+        'lg' => 1200,
+    ];
+
+    const MAX_WIDTH = 1920;
+
     public static function removeByName(string $name, string $path): void
     {
         $mainName = explode('.', $name)[0]; 
