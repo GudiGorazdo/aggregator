9665960721dcfaf99b1b58a16653cc74d3f83655
oseminikhin <oseminikhin@tolkit.top>
2023-06-07 16:20:32 +0300

    shop chains migration created; DataParser update

--- app/Console/Commands/DataParser.php
+++ app/Console/Commands/DataParser.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 
 class DataParser extends Command
 {
@@ -21,12 +23,27 @@ class DataParser extends Command
     protected $description = 'Create database fom data folder';
 
     /**
+     * Путь к папке с данными из сервисов
+     *
+     * @var string
+     */
+    protected $dataPath;
+
+    public function __construct() {
+        parent::__construct();
+        $this->dataPath = $_SERVER['DOCUMENT_ROOT'] . '/data';
+    }
+
+    /**
      * Execute the console command.
      *
      * @return int
      */
     public function handle()
     {
+        //$x = new IOFactory();
+        var_dump(class_exists('Command'));
+        echo $this->dataPath . PHP_EOL;
         echo 'ok' . PHP_EOL;
         return Command::SUCCESS;
     }
