17fd8edf11b2720dcef7f1902f02fdee717a4f0a
oseminikhin <oseminikhin@tolkit.top>
2023-06-07 20:29:41 +0300

    parser update

--- app/Console/Commands/DataParser.php
+++ app/Console/Commands/DataParser.php
@@ -27,6 +27,13 @@ class DataParser extends Command
     protected $description = 'Create database fom data folder';
 
     /**
+    * Путь до папки сервиса
+    *
+    * @var string
+    */
+    protected string $serviceFolder = '';
+
+    /**
     * Название файла основной таблицы сервиса
     *
     * @var string
@@ -41,13 +48,13 @@ class DataParser extends Command
     protected string $dataPath = '/data';
 
     /**
-     * Список полей в таблице
-     * Порядок важен (!) он соответствует порядку столбцов в файле таблицы.
+     * Список полей в основной таблице
+     * В поле subtables лежат ссылки на методы,
+     * которые получают данные из ссылок на файлы
      *
      * @var string[]
      */
-
-    protected array $cells = [
+    protected array $cellsMainTable = [
         'A' => 'link',
         'B' => 'id',
         'C' => 'branches',
@@ -75,7 +82,26 @@ class DataParser extends Command
         'Y' => 'photos',
         'Z' => 'rating',
         'AA' => 'respones',
+        'subtables' => [
+            'C' => 'getBranchesData',
+            'M',
+            'N',
+            'P',
+            'Q',
+            'U',
+            'V',
+            'W',
+            'AA'
+        ],
     ];
+
+    /**
+     * Список полей в таблице филиалов
+     * Порядок важен (!) он соответствует порядку столбцов в файле таблицы.
+     *
+     * @var string[]
+     */
+    protected array $cellsBranchesTable = [ 'A' => 'link' ];
 
     /**
      * Execute the console command.
@@ -85,80 +111,93 @@ class DataParser extends Command
     public function handle()
     {
         foreach (Service::all() as $service) {
-            $serviceFolder = $this->getServiceFolderPath($service->name);
-            $tableFile = $this->getTableFilePath($serviceFolder);
+            $this->serviceFolder = $this->getServiceFolderPath($service->name);
+            $tableFile = $this->getTableFilePath();
 
-            if (!is_dir($serviceFolder)) {
-                echo "Произошла ошибка: папки $serviceFolder не существует" . PHP_EOL;
-                return Command::FAILURE;
-            }
-            if (!file_exists($tableFile)) {
-                echo "Произошла ошибка: файла $tableFile не существует" . PHP_EOL;
-                return Command::FAILURE;
+            if (!is_dir($this->serviceFolder)) {
+                echo "Произошла ошибка: папки $this->serviceFolder не существует" . PHP_EOL;
+                return Command::SUCCESS;
             }
 
-            $spreadsheet = IOFactory::load($tableFile);
-            $worksheet = $spreadsheet->getActiveSheet();
-            $fileData = $this->getFileData($worksheet);
-            //$data = $this->createDataArray($fileData);
+            $fileData = $this->getFileData($tableFile);
 
 
             dump($fileData[1]);
-            //dump($data[1]);
-            //dump(scandir($serviceFolder));
-
-            //fclose($spreadsheet->getStream());
-            //$spreadsheet->disconnectAllContainers();
-            unset($spreadsheet);
         }
 
         return Command::SUCCESS;
     }
 
-    private function createDataArray(array $fileData): array
+    private function getBranchesData(string $filePath): array
     {
         $data = [];
-        foreach ($fileData as $row) {
-            //$rowData =
-            foreach($row as $ind => $cell) {
-                dd($row[$ind]);
-            }
-        }
-        return $data;
+        if (!$filePath) return $data;
+
+        return $this->getFileData($filePath, 'branches');
+
     }
 
-    private function getRowData(Row $row): array
+    private function getRowData(Row $row, string $type): array
     {
+        $cellsMap = $this->getCellsMap($type);
         $rowData = [];
         foreach ($row->getCellIterator() as $ind => $cell) {
             $cellValue = $cell->getValue();
+            $cellName = $cellsMap[$ind];
             if (!($cellValue instanceof RichText)) {
-                $rowData[$this->cells[$ind]] = $cellValue;
+                $rowData[$cellName] = $cellValue;
                 continue;
             }
 
             $richTextElements = $cellValue->getRichTextElements();
 
-            if (!empty($richTextElements)) {
-                $rowData[$this->cells[$ind]] = $richTextElements[0]->getText();
-            } else {
-                $rowData[$this->cells[$ind]] = null;
+            if (empty($richTextElements)) {
+                $rowData[$cellName] = null;
+                continue;
             }
+
+            $text = $richTextElements[0]->getText();
+            if (isset($cellsMap['subtables']) && isset($cellsMap['subtables'][$ind])) {
+                $subTablePath = $this->getSubTableFilePath($text);
+                $method = $cellsMap['subtables'][$ind];
+                $rowData[$cellName] = $this->{ $method }($subTablePath);
+                continue;
+            }
+
+            $rowData[$cellName] = $text;
         }
 
         return $rowData;
     }
 
-    private function getFileData(Worksheet $worksheet): array
+    private function getFileData(string $filePath, string $type = 'main'): array|false
     {
+        if (!file_exists($filePath)) {
+            echo "Произошла ошибка: файла $filePath не существует" . PHP_EOL;
+            return false;
+        }
+
         $data = [];
+        $spreadsheet = IOFactory::load($filePath);
+        $worksheet = $spreadsheet->getActiveSheet();
         foreach ($worksheet->getRowIterator() as $ind => $row) {
             if ($ind === 1 ) continue;
-            $rowData = $this->getRowData($row);
+            $rowData = $this->getRowData($row, $type);
             $data[] = $rowData;
         }
 
+        unset($spreadsheet);
         return $data;
+    }
+
+    private function getCellsMap(string $type): array
+    {
+        switch ($type) {
+            case 'main':
+                return $cellsMap = $this->cellsMainTable;
+            case 'branches':
+                return $cellsMap = $this->cellsBranchesTable;
+        }
     }
 
     private function getServiceFolderPath(string $name): string
@@ -166,9 +205,14 @@ class DataParser extends Command
         return base_path() . $this->dataPath . '/' . $name;
     }
 
-    private function getTableFilePath(string $serviceFolder): string
+    private function getTableFilePath(): string
     {
-        return $serviceFolder . '/' . $this->tableFileName;
+        return $this->serviceFolder . '/' . $this->tableFileName;
+    }
+
+    private function getSubTableFilePath(string $subTableFile): string
+    {
+        return $this->serviceFolder . '/' . $subTableFile;
     }
 }
 
