5259b09caa178230410f77ffeb2c899801707851
oseminikhin <oseminikhin@tolkit.top>
2023-06-07 18:29:10 +0300

    parser update

--- app/Console/Commands/DataParser.php
+++ app/Console/Commands/DataParser.php
@@ -5,6 +5,10 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\RichText\RichText;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Worksheet\Row;
+use \App\Models\Service;
 
 class DataParser extends Command
 {
@@ -23,16 +27,55 @@ class DataParser extends Command
     protected $description = 'Create database fom data folder';
 
     /**
-     * Путь к папке с данными из сервисов
+    * Название файла основной таблицы сервиса
+    *
+    * @var string
+    */
+    protected $tableFileName = 'data.xlsx';
+
+    /**
+     * Путь к папке(относительно корня проекта) с данными из сервисов
      *
      * @var string
      */
-    protected $dataPath;
+    protected $dataPath = '/data';
 
-    public function __construct() {
-        parent::__construct();
-        $this->dataPath = $_SERVER['DOCUMENT_ROOT'] . '/data';
-    }
+    /**
+     * Список полей в таблице
+     * Порядок важен (!) он соответствует порядку столбцов в файле таблицы.
+     *
+     * @var array
+     */
+
+    protected $cells = [
+        'link',
+        'id',
+        'branches',
+        'name',
+        'description',
+        'activity',
+        'region',
+        'city',
+        'municipality',
+        'area',
+        'address',
+        'zip',
+        'nearest_metro',
+        'nearest_stops',
+        'coordinates',
+        'phones',
+        'web',
+        'whatsapp',
+        'telegram',
+        'vk',
+        'add_socials',
+        'mail',
+        'working_mode',
+        'logo',
+        'photos',
+        'rating',
+        'respones',
+    ];
 
     /**
      * Execute the console command.
@@ -41,11 +84,75 @@ class DataParser extends Command
      */
     public function handle()
     {
-        //$x = new IOFactory();
-        var_dump(class_exists('Command'));
-        echo $this->dataPath . PHP_EOL;
-        echo 'ok' . PHP_EOL;
+        foreach (Service::all() as $service) {
+            $serviceFolder = $this->getServiceFolderPath($service->name);
+            $tableFile = $this->getTableFilePath($serviceFolder);
+
+            if (!is_dir($serviceFolder)) {
+                echo "Произошла ошибка: папки $serviceFolder не существует" . PHP_EOL;
+                return Command::FAILURE;
+            }
+            if (!file_exists($tableFile)) {
+                echo "Произошла ошибка: файла $tableFile не существует" . PHP_EOL;
+                return Command::FAILURE;
+            }
+
+            $spreadsheet = IOFactory::load($tableFile);
+            $worksheet = $spreadsheet->getActiveSheet();
+            $data = $this->getFileData($worksheet);
+
+            dump($data[1]);
+            //dump(scandir($serviceFolder));
+
+            //fclose($spreadsheet->getStream());
+            //$spreadsheet->disconnectAllContainers();
+            unset($spreadsheet);
+        }
+
         return Command::SUCCESS;
+    }
+
+    private function getRowData(Row $row): array
+    {
+        $rowData = [];
+        foreach ($row->getCellIterator() as $cell) {
+            $cellValue = $cell->getValue();
+            if (!($cellValue instanceof RichText)) {
+                $rowData[] = $cellValue;
+                continue;
+            }
+
+            $richTextElements = $cellValue->getRichTextElements();
+
+            if (!empty($richTextElements)) {
+                $rowData[] = $richTextElements[0]->getText();
+            } else {
+                $rowData[] = null;
+            }
+        }
+
+        return $rowData;
+    }
+
+    private function getFileData(Worksheet $worksheet): array
+    {
+        $data = [];
+        foreach ($worksheet->getRowIterator() as $row) {
+            $rowData = $this->getRowData($row);
+            $data[] = $rowData;
+        }
+
+        return $data;
+    }
+
+    private function getServiceFolderPath(string $name): string
+    {
+        return base_path() . $this->dataPath . '/' . $name;
+    }
+
+    private function getTableFilePath(string $serviceFolder): string
+    {
+        return $serviceFolder . '/' . $this->tableFileName;
     }
 }
 
