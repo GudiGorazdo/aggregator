802fa48714f00e804f458d8a42297e245f4cf313
oseminikhin <oseminikhin@tolkit.top>
2023-09-20 09:41:02 +0300

    hero adaptive

--- resources/js/scripts/layouts/hero.js
+++ resources/js/scripts/layouts/hero.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', (e) => {
-  const heroText = document.querySelector(".hero-text-box");
+  const heroText = document.querySelector(".hero__text-box");
   const heroTextBtn = document.querySelector(
-    ".hero-text-expand-btn"
+    ".hero__text-expand-btn"
   );
   heroTextBtn.addEventListener("click", () => {
     heroText.classList.toggle("active");
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -16,24 +16,24 @@
 
 @section('content')
 <section class="hero container-wide mb-45">
-    <div class="hero-content flex-ctr">
-        <h1 class="hero-title">{{ $title }}</h1>
-        <div class="hero-text-box">
-            <p class="hero-text">
+    <div class="hero__content flex-ctr">
+        <h1 class="hero__title">{{ $title }}</h1>
+        <div class="hero__text-box">
+            <p class="hero__text">
                 Срочный выкуп ноутбуков, телефонов, планшетов, фотоаппаратов в
                 любом состоянии.
             </p>
-            <p class="hero-text">
+            <p class="hero__text">
                 Срочный выкуп ноутбуков, телефонов, планшетов, фотоаппаратов в
                 любом состоянии.
             </p>
-            <p class="hero-text">
+            <p class="hero__text">
                 Срочный выкуп ноутбуков, телефонов, планшетов, фотоаппаратов в
                 любом состоянии.
             </p>
-            <button class="btn hero-text-expand-btn"></button>
+            <button class="btn hero__text-expand-btn"></button>
         </div>
-        <a class="btn btn--primary hero-btn" href="#">Отправить заявку всем</a>
+        <a class="btn btn--primary hero__btn" href="#">Отправить заявку всем</a>
     </div>
 </section>
 
