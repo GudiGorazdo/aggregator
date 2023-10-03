bfd809ea9109e9636e86c94762e6385397f467f7
oseminikhin <oseminikhin@tolkit.top>
2023-10-03 20:42:49 +0300

    shop info block adaptive

--- resources/views/layouts/hours.blade.php
+++ resources/views/layouts/hours.blade.php
@@ -1,55 +1,59 @@
 <table class="hours">
     <thead>
         <tr>
-            <th>ПН</th>
-            <th>ВТ</th>
-            <th>СР</th>
-            <th>ЧТ</th>
-            <th>ПТ</th>
-            <th class="hours__weekend">СБ</th>
-            <th class="hours__weekend">ВС</th>
+            @foreach($workingMode as $day)
+                @if ($day->day_of_week > 5)
+                <th class="hours__weekend">
+                    {{ \App\Services\DayService::getDayByNum($day->day_of_week) }}
+                </th>
+                @else
+                <th>
+                    {{ \App\Services\DayService::getDayByNum($day->day_of_week) }}
+                </th>
+                @endif
+            @endforeach
         </tr>
     </thead>
     <tbody>
         <tr>
             @foreach($workingMode as $day)
-                <td>
-                    {{-- <div class="hours__dot hours__dot--active">&nbsp;</div> --}}
-                    @if($day->is_open)
-                        <div class="hours__dot">&nbsp;</div>
-                    @endif
-                </td>
+            <td>
+                {{-- <div class="hours__dot hours__dot--active">&nbsp;</div> --}}
+                @if($day->is_open)
+                <div class="hours__dot">&nbsp;</div>
+                @endif
+            </td>
             @endforeach
         </tr>
         <tr>
             @foreach($workingMode as $day)
-                <td>{{ $day->is_open ? ($day->open_time ? \Carbon\Carbon::parse($day->open_time)->format('H:i') : '00:00'): 'Выходной' }}</td>
-            @endforeach
-        </tr>
-
-        <tr>
-            @foreach($workingMode as $day)
-                <td>
-                    @if($day->is_open)
-                        <div class="hours__dot">&nbsp;</div>
-                    @endif
-                </td>
+            <td>{{ $day->is_open ? ($day->open_time ? \Carbon\Carbon::parse($day->open_time)->format('H:i') : '00:00'): 'Выходной' }}</td>
             @endforeach
         </tr>
 
         <tr>
             @foreach($workingMode as $day)
-                <td>{{ $day->is_open ? ($day->close_time ? \Carbon\Carbon::parse($day->close_time)->format('H:i') : '23:59') : '' }}</td>
+            <td>
+                @if($day->is_open)
+                <div class="hours__dot">&nbsp;</div>
+                @endif
+            </td>
             @endforeach
         </tr>
 
         <tr>
             @foreach($workingMode as $day)
-                <td>
-                    @if($day->is_open)
-                        <div class="hours__dot">&nbsp;</div>
-                    @endif
-                </td>
+            <td>{{ $day->is_open ? ($day->close_time ? \Carbon\Carbon::parse($day->close_time)->format('H:i') : '23:59') : '' }}</td>
+            @endforeach
+        </tr>
+
+        <tr>
+            @foreach($workingMode as $day)
+            <td>
+                @if($day->is_open)
+                <div class="hours__dot">&nbsp;</div>
+                @endif
+            </td>
             @endforeach
         </tr>
     </tbody>
@@ -57,34 +61,25 @@
 
 <table class="hours hours--mobile">
     <tbody>
-        <tr>
-            <th>ПН</th>
-            <td>09:00 - 21:00</td>
-        </tr>
-        <tr>
-            <th>ВТ</th>
-            <td>09:00 - 21:00</td>
-        </tr>
-        <tr>
-            <th>СР</th>
-            <td>09:00 - 21:00</td>
-        </tr>
-        <tr>
-            <th>ЧТ</th>
-            <td>09:00 - 21:00</td>
-        </tr>
-        <tr>
-            <th>ПТ</th>
-            <td>09:00 - 21:00</td>
-        </tr>
-        <tr class="hours__weekend">
-            <th>СБ</th>
-            <td>09:00 - 21:00</td>
-        </tr>
-        <tr class="hours__weekend">
-            <th>ВС</th>
-            <td>Выходной</td>
-        </tr>
+        @foreach($workingMode as $day)
+            @if ($day->day_of_week > 5)
+            <tr class="hours__weekend">
+                <th>
+                    {{ \App\Services\DayService::getDayByNum($day->day_of_week) }}
+                </th>
+            @else
+            <tr>
+                <th>
+                    {{ \App\Services\DayService::getDayByNum($day->day_of_week) }}
+                </th>
+            @endif
+                <td>
+                    {{ $day->is_open ? ($day->open_time ? \Carbon\Carbon::parse($day->open_time)->format('H:i') : '00:00'): 'Выходной' }}
+
+                    {{ $day->is_open ? ($day->close_time ? ' - ' . \Carbon\Carbon::parse($day->close_time)->format('H:i') : ' - 23:59') : '' }}
+                </td>
+            </tr>
+        @endforeach
     </tbody>
 </table>
 
