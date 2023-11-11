d5e3eab4fdf3e354a657e9804696d360332759d2
method <semenikhinoleg777@yandex.ru>
2023-11-11 09:50:15 +0300

    styles update

--- resources/views/layouts/similar-companies.blade.php
+++ resources/views/layouts/similar-companies.blade.php
@@ -1,16 +1,18 @@
 <section class="similar-companies container">
     <h2 class="similar-companies__title">Похожие компании</h2>
 
-    <x-carousel classMod='similar-companies' alt="фото компании" >
-        @foreach ($similars as $similar)
-            <x-carousel-item classMod="similar-companies" >
-                @include('layouts.similar-company-card', ['similar' => $similar])
-            </x-carousel-item>
-        @endforeach
-    </x-carousel>
-    <div class="similar-companies__navigation">
-        <div class="btn forwards similar-companies-previous"><x-icon-slider-arrow-left /></div>
-        <div class="btn previous similar-companies-forwards"><x-icon-slider-arrow-right /></div>
+    <div class="similar-companies__carousel">
+        <x-carousel classMod='similar-companies' alt="фото компании" >
+            @foreach ($similars as $similar)
+                <x-carousel-item classMod="similar-companies" >
+                    @include('layouts.similar-company-card', ['similar' => $similar])
+                </x-carousel-item>
+            @endforeach
+        </x-carousel>
+        <div class="similar-companies__navigation">
+            <div class="btn forwards similar-companies-previous"><x-icon-slider-arrow-left /></div>
+            <div class="btn previous similar-companies-forwards"><x-icon-slider-arrow-right /></div>
+        </div>
     </div>
 </section>
 
