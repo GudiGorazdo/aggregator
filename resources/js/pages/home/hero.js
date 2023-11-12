09efa199fb8ff1a63f4a272eb7a26fc4b52b7fad
method <semenikhinoleg777@yandex.ru>
2023-11-13 01:13:55 +0300

    site-messaage styles update

--- resources/js/pages/home/hero.js
+++ resources/js/pages/home/hero.js
@@ -2,6 +2,6 @@ import "../../scripts/expand";
 
 document.querySelectorAll('.hero__btn').forEach(button => {
 		button.addEventListener('click', (e) => {
-			app.modal.showMessage({text: 'Данный функционал находится в разработке'});
+			app.modal.showMessage({text: 'Данный функционал находится в разработке', title: 'Тестовый заголовок'});
 		});
 	})
