68e1278f5467934c1f2a1361f1299c5cde4baed4
method <semenikhinoleg777@yandex.ru>
2023-11-17 13:06:10 +0300

    category filter ui update

--- resources/js/layouts/aside.js
+++ resources/js/layouts/aside.js
@@ -1,20 +1,28 @@
 const categoryUI = {
-  classes: { active: 'active' },
+  classes: { active: 'active', },
   categoryList: document.querySelector(".search--filter"),
   toggleCategoryListBtn: document.getElementById("toggle-category"),
   applyCategory: document.querySelector('.search--filter .search__btn--selection'),
   // actionButtons: document.querySelectorAll('.search--filter  .search__btn'),
 
   init() {
-    [this.toggleCategoryListBtn, this.applyCategory].forEach(button => {
-      button.addEventListener("click", () => {
-        this.toggleActive(this.categoryList);
-        this.toggleActive(this.toggleCategoryListBtn);
-      });
+    this.toggleCategoryListBtn.addEventListener("click", () => {
+      this.toggleActive(this.categoryList);
+      this.toggleActive(this.toggleCategoryListBtn);
+    });
+    this.applyCategory.addEventListener("click", () => {
+      this.categoryList.classList.remove(this.classes.active);
+      this.toggleCategoryListBtn.classList.remove(this.classes.active);
+      this.closeSubCategoriesList();
     });
   },
 
   toggleActive: function (element) {
     element.classList.toggle(this.classes.active);
   },
+
+  closeSubCategoriesList() {
+    const list = document.querySelector('.categories-list--filter .categories-list__item .categories-list__brands.open');
+    list && list.classList.remove('open');
+  }
 }.init();
